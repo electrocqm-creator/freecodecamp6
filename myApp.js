77ad981287cabeff8d2ea70214a03678f2474fc3
electrocqm-creator <electrocqm@gmail.com>
2025-10-06 00:16:42 -0300

    Fix syntax errors in done() functions

--- myApp.js
+++ myApp.js
@@ -23,47 +23,47 @@ const Person = mongoose.model("Person", personSchema);
 // ==== No modifiques lo que sigue hasta que FreeCodeCamp lo pida ====
 
 const createAndSavePerson = (done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  done(null /, data/);
+  done(null /*, data*/);
 };
 
 // ✅ Export model for FreeCodeCamp tests
@@ -77,6 +77,6 @@ exports.findAndUpdate = findAndUpdate;
 exports.createManyPeople = createManyPeople;
 exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+exports.queryChain = queryChain;
 
 
