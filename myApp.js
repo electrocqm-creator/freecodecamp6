f433fe9122d1c60de1f2aeb26fccbd86723a825c
electrocqm-creator <electrocqm@gmail.com>
2025-10-06 22:21:27 -0300

    encontrar persona

--- myApp.js
+++ myApp.js
@@ -64,7 +64,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
